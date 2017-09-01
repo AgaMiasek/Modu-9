4300e2774fbda99ab68387f775c7b48bebb493a4
AgaMiasek <ag.miasek@gmail.com>
2017-09-01 20:34:57 +0200

    dodanie plikow do zadania 2

---
+++ 9.2/scripts.js
@@ -0,0 +1,15 @@
+var femaleNames = ['Ania', 'Basia', 'Ola', 'Jola', 'Kasia'];
+var maleNames = ['Arek', 'Jarek', 'Olek', 'Darek', 'Wojtek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Maniek'
+
+  if (allNames.indexOf(newName) === -1) {
+	 
+	console.log(allNames.push(newName));
+	}
+console.log(allNames);
+else {'imie już jest'}
