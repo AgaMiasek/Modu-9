2529ca0492325ae25de533a6af56170b357dfcf8
pmiasek <pmiasek@ce5.pl>
2017-09-01 15:05:33 +0200

    poprawka zadania

--- zad 9.1/js/scripts.js
+++ zad 9.1/js/scripts.js
@@ -5,11 +5,11 @@ var allNames = femaleNames.concat(maleNames);
 
 console.log(allNames);
 
-var newName = '0la'
-var x = allNames.push(newName);
+var newName = 'Basia'
 
   if (allNames.indexOf(newName) === -1) {
-	console.log(x);
+	 
+	console.log(allNames.push(newName));
 	}
 console.log(allNames);
 
