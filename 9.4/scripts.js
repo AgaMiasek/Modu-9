3ab7f3cf49d8b2dcaec53b993da70c9025cf11f1
Aga <ag.miasek@gmail.com>
2017-09-05 19:37:44 +0200

    dodanie zadanie 9.4

---
+++ 9.4/scripts.js
@@ -0,0 +1,14 @@
+
+function drawTree(n) {
+	
+    for (i=0; i<=n; i++) {
+		var star = "";
+        for(j=0; j<=i; j++)
+    {
+    star += '*';
+    }
+    console.log(star);
+
+    }
+}
+console.log(drawTree(5));
