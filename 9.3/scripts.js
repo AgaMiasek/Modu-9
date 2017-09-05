05b45feb033261ad3da89d0701e6677b40daf79b
Aga <ag.miasek@gmail.com>
2017-09-05 18:37:29 +0200

    tylko pierwsze słowo uppercase

--- 9.3/scripts.js
+++ 9.3/scripts.js
@@ -1,15 +1,17 @@
+
 var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 var dinosaur = 'Triceratops';
 
-var textCharsAfter = text.replace('Velociraptor',dinosaur);
-
-//console.log(textCharsAfter);
-
-var upperCase = textCharsAfter.toUpperCase();
+var upperCase = dinosaur.toUpperCase();
 
 //console.log(upperCase);
 
-var upperCaseAfter =upperCase.substr(0, upperCase.length/2);
+var textCharsAfter = text.replace('Velociraptor',upperCase);
+
+//console.log(textCharsAfter);
+
+
+var upperCaseAfter = textCharsAfter.substr(0, textCharsAfter.length/2);
 
 console.log(upperCaseAfter);
 
