b3463cd9c788e86a77bed7b4d6006e99ddb0e454
Michał <michal.bootcamp@gmail.com>
2017-09-06 23:51:13 +0200

    dodanie html, formatowanie js

--- 9.4/scripts.js
+++ 9.4/scripts.js
@@ -3,12 +3,10 @@ function drawTree(n) {
 	
     for (i=0; i<=n; i++) {
 		var star = "";
-        for(j=0; j<=i; j++)
-    {
-    star += '*';
-    }
-    console.log(star);
-
+        for (j=0; j<=i; j++) {
+            star += '*';
+        }
+        console.log(star);
     }
 }
 console.log(drawTree(5));
